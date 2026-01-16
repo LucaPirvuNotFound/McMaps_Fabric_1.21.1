ab0d1f20f3eb9e1ee323743545f2bfd4697ff670
BrainNotFound404 <luca.pirvu@gmail.com>
2026-01-16 02:08:27 +0200

    Initial commit

---
+++ src/main/java/net/ralubog/mcmaps_maze/McMaps_MazeClient.java
@@ -0,0 +1,10 @@
+package net.ralubog.mcmaps_maze;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McMaps_MazeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ralubog/mcmaps_maze/McMaps_MazeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ralubog.mcmaps_maze;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class McMaps_MazeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
